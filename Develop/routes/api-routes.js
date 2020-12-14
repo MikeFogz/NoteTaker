ec4464510133c1e13fde1fdc41e870919a360d59
MikeFogz <MikeFogz@gmail.com>
2020-12-13 22:15:39 -0800

    UPDATE: Initial server connection made. client route get requests working properly.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,4 +1,15 @@
 const router = require("express").Router();
+const fs = require("fs");
 
+router.get("/api/notes", (req, res) => {
+    fs.readFile("./data.json", "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+router.post("/api/notes", (req, res) => {
+    console.log(req.body);
+});
 
 module.exports = router
--- Develop/server.js
+++ Develop/server.js
@@ -5,10 +5,10 @@ const PORT = 5000;
 const apiRoutes = require("./routes/api-routes");
 const clientRoutes = require("./routes/client-routes");
 
-app.use(express.urlencodeed({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use("/api", apiRoutes);
 app.use("/", clientRoutes);
 
-app.listen(PORT, () => console.log(`listending at http:localhost:${PORT}`));
+app.listen(PORT, () => console.log(`listending at http://localhost:${PORT}`));
