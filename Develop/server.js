17618a30af0be508a1dc0d2f92bdfb38d5d309dd
MikeFogz <MikeFogz@gmail.com>
2020-12-17 00:13:19 -0800

    added express.static

--- Develop/server.js
+++ Develop/server.js
@@ -7,6 +7,8 @@ const clientRoutes = require("./routes/client-routes");
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"))
+
 
 app.use("/api", apiRoutes);
 app.use("/", clientRoutes);
